13588419ee473118c85dcc840139633d23ebc70b
sq3tle <sq3tle@gmail.com>
2022-12-04 16:48:26 +0100

    alpha

--- layer0/phy.py
+++ layer0/phy.py
@@ -33,10 +33,12 @@ class RadioEnvironment(object):
 
 
 class PhysicalLayer:
-    def __init__(self, name, env, rf):
+    def __init__(self, name, env, rf, out):
+
         self.id = name
         self.env = env
         self.geo = None
+        self.out = out
 
         self.rf = rf
         self.rf_pipe = rf.rx()
@@ -54,6 +56,7 @@ class PhysicalLayer:
                     "time_toa": time_toa,
                     "sender": self,
                     "payload": data})
+        self.out.add_packet(self.env.now, {"source": self.id}, 0)
 
         yield self.env.timeout(time_toa)
 
@@ -95,17 +98,30 @@ class PhysicalLayer:
                        'snr': link.calculate_snr(msg["sender"].tx_power),
                        'toa': msg['time_toa'],
                        'payload': msg["payload"]}
-
-                if utils.monte_carlo(self.rf.SF, msg['snr'], len(msg['payload']) + self.rf.HEADER):
-                    yield self.env.timeout(msg["toa"])
-                    # self._log(msg, state="lost\t")
-                    continue
+                msg['lost'] = utils.monte_carlo(self.rf.SF, msg['snr'], len(msg['payload']) + self.rf.HEADER)
 
                 yield self.env.timeout(msg["toa"])
 
                 if self.rx:
                     self.received.append(msg)
-                    self._log(msg['payload'], state="received")
+                    packet = msg.copy()
+                    self.out.add_packet(self.env.now, {
+                        "tx": packet["sender"],
+                        "rx": self.id,
+                        "source": packet['payload']["from"],
+                        "destination": packet['payload']["dest"],
+                        "hops": [str(hop) for hop in packet['payload']["hops"]],
+                        "rssi": packet['rssi'],
+                        "snr": packet['snr'],
+                        "duration": packet['toa'],
+                        "payload": packet['payload']['payload'],
+                        "lost": packet['lost']
+                    }, packet['toa'])
+
+                    if packet['lost']:
+                        self._log(msg['payload'], state="lost\t")
+                    else:
+                        self._log(msg['payload'], state="received")
 
     def _log(self, msg, state="", warn=False):
         log = logger.warning if warn else logger.info
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from random import choice, randint
+from random import randint
 
 import simpy
 from scipy import interpolate
@@ -39,24 +39,33 @@ class Transmission:
 class Device:
     env = None
     rf = None
+    out = None
 
     def __init__(self, name):
 
         if not self.env or not self.rf:
             raise Exception("Please specify simulation and rf environments")
 
-        self.phy = PhysicalLayer(name, self.env, self.rf)
+        self.phy = PhysicalLayer(name, self.env, self.rf, self.out)
         self.transmissions = []
         self.path = None
         self.neighbors = None
         self.tx_power = 14
-        self.env.process(self.update_device())
 
     def update_device(self):
-        if self.neighbors:
-            self.neighbors = list(filter(lambda x: x['last_heard'] > self.env.now + 5000, self.neighbors))
-        self.phy.geo = self.path.get_location(self.env.now)
-        yield self.env.timeout(1)
+        while 1:
+            if self.neighbors:
+                self.neighbors = list(filter(lambda x: x['last_heard'] > self.env.now + 5000, self.neighbors))
+            self.phy.geo = self.path.get_location(self.env.now)
+
+            self.out.add_device(self.env.now, {
+                "name": self.phy.id,
+                "lat": float(self.phy.geo[0]),
+                "lon": float(self.phy.geo[1]),
+                "alt": float(self.phy.geo[2]),
+            })
+
+            yield self.env.timeout(1)
 
     def test_loop(self):
         self.env.process(self.update_device())
@@ -81,61 +90,126 @@ class Device:
                         yield self.env.process(self.phy.tx(packet['payload']))
 
 
+class DataInterface:
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = []
+        self.last_chunk_end = 0
+        self.last_chunk = 0
 
-def parse_input(environment, filename):
-    devices = []
-    with open(filename) as f_input:
-        sim_setup = json.loads(f_input.read())
+    def add_packet(self, time, data, lenght):
+        chunk_time = time - self.last_chunk_end
+        self._extend(chunk_time)
 
-    if sim_setup.get('settings', False):
-        environment.rf.SF = sim_setup['settings'].get('spreading_factor', 7)
-        environment.rf.BW = sim_setup['settings'].get('bandwidth', 0.125)
-        environment.rf.HEADER = sim_setup['settings'].get('phy_header_length', 11)
-    else:
-        raise Exception('input json no settings section present')
+        if not self.data[chunk_time - lenght].get('packets', False):
+            self.data[chunk_time - lenght]['packets'] = []
 
-    if sim_setup.get('devices', False):
-        for device in sim_setup.get('devices'):
+        self.data[chunk_time - lenght]['packets'].append(data)
 
-            if device.get('id', False):
-                new_device = Device(device.get('id'))
-            else:
-                raise Exception('input json no device name present in device definition')
+    def add_link(self, time, data):
+        chunk_time = time - self.last_chunk_end
+        self._extend(chunk_time)
 
-            new_device.tx_power = device.get('tx_power', 14)
+        if not self.data[chunk_time].get('links', False):
+            self.data[chunk_time]['links'] = []
 
-            if device.get('path', False):
-                path = device.get('path')
-                if len(path) == 0:
-                    raise Exception('input json device path empty')
-                new_device.path = Path(path)
-            else:
-                raise Exception('input json device no path section')
+        self.data[chunk_time]['links'].append(data)
 
-            if device.get('transmissions', False):
-                transmissions = device.get('transmissions')
-                if len(transmissions) != 0:
-                    for transmission in transmissions:
-                        new_device.transmissions.append(
-                            Transmission(transmission.get('data', ''), transmission.get('interval', 1000),
-                                         transmission.get('lenght', 0), transmission.get('destination', 'all'))
-                        )
+    def add_device(self, time, data):
+        chunk_time = time - self.last_chunk_end
+        self._extend(chunk_time)
 
-            devices.append(new_device)
+        if not self.data[chunk_time].get('devices', False):
+            self.data[chunk_time]['devices'] = []
 
-    else:
-        raise Exception('input json no devices section present')
+        self.data[chunk_time]['devices'].append(data)
 
-    return devices
+    def commit(self):
+
+        last = None
+        self.last_chunk_end += len(self.data)
+        self.last_chunk += 1
+
+        for step in self.data.copy():
+            now = self._datastr(step)
+            if last == now:
+                self.data.remove(step)
+                continue
+            last = now
+
+        with open("{}_{}.json".format(self.filename, self.last_chunk), "w") as f:
+            f.write(json.dumps(self.data, indent=2))
+        self.data = []
+
+    def _extend(self, time):
+        for i in range(len(self.data), time + 1):
+            self.data.append({"time": i + self.last_chunk_end})
+
+    def _datastr(self, json):
+        return "{} {} {}".format(json.get('packets', ''), json.get('devices', ''), json.get('links', ''))
+
+    @staticmethod
+    def parse_input(environment, filename):
+        devices = []
+        with open(filename, "r") as f_input:
+            sim_setup = json.loads(f_input.read())
+
+        if sim_setup.get('settings', False):
+            environment.rf.SF = sim_setup['settings'].get('spreading_factor', 7)
+            environment.rf.BW = sim_setup['settings'].get('bandwidth', 0.125)
+            environment.rf.HEADER = sim_setup['settings'].get('phy_header_length', 11)
+        else:
+            raise Exception('input json no settings section present')
+
+        if sim_setup.get('devices', False):
+            for device in sim_setup.get('devices'):
+
+                if device.get('id', False):
+                    new_device = Device(device.get('id'))
+                else:
+                    raise Exception('input json no device name present in device definition')
+
+                new_device.tx_power = device.get('tx_power', 14)
+
+                if device.get('path', False):
+                    path = device.get('path')
+                    if len(path) == 0:
+                        raise Exception('input json device path empty')
+                    new_device.path = Path(path)
+                else:
+                    raise Exception('input json device no path section')
+
+                if device.get('transmissions', False):
+                    transmissions = device.get('transmissions')
+                    if len(transmissions) != 0:
+                        for transmission in transmissions:
+                            new_device.transmissions.append(
+                                Transmission(transmission.get('data', ''), transmission.get('interval', 1000),
+                                             transmission.get('lenght', 0), transmission.get('destination', 'all'))
+                            )
+
+                devices.append(new_device)
+
+        else:
+            raise Exception('input json no devices section present')
+
+        return devices
 
 
-if __name__ == "__main__":
+def basic_use(input, output):
     Environment = Device  # for more clarity
     Environment.env = simpy.Environment()
     Environment.rf = RadioEnvironment(Environment.env)
+    Environment.out = DataInterface(output)
 
-    devices = parse_input(Environment, "input.json")
+    devices = DataInterface.parse_input(Environment, input)
     for device in devices:
         Environment.env.process(device.test_loop())
 
-    Environment.env.run(until=10 * 1000)
+    for i in range(1, 2 + 1):
+        Environment.env.run(until=30000 * i)
+        Environment.out.commit()
+
+
+if __name__ == "__main__":
+    basic_use("example_static_3devices_ping.json", "example")
